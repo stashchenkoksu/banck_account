1220d8852763f2a8f57c8b4b524cd4f9d8796599
Stashchenko Kseniya <kstashchenka@aristeksystems.com>
2021-11-04 14:43:38 +0300

    fix reports

--- README.md
+++ README.md
@@ -22,9 +22,28 @@ $ git clone https://github.com/stashchenkoksu/banck_account
 $ bundle install
 ~~~
 
+**Setup database with:**
+>make sure you have postgress sql installed and running on your system
+~~~bash
+$ rails db:create
+$ rails db:migrate
+$ rails db:seed   # install sample list data
+~~~
+
 **Start server with:**
 ~~~bash
 $ rails s
+~~~
+
+**Install project manager**
+~~~bash
+$ yarn or npm install
+~~~
+
+**To get reports**
+~~~bash
+$ rake 'reports:first_report[:user_identify_number, time_period]'
+other statistic you can get edititng sorce code(
 ~~~
 
 This app should be running at `http://localhost:3000/`
--- app/services/report_creator.rb
+++ app/services/report_creator.rb
@@ -9,11 +9,13 @@ class ReportCreator < ApplicationService
   def call
     case @type
     when '1'
-      unless @params.nil?
+      if @params.nil?
+        deposition_amount
+      else
         user_ids = @params[:user_ids]
         time_period = @params[:time_period]
+        deposition_amount(user_ids, time_period)
       end
-      deposition_amount(user_ids, time_period)
     when '2'
       extremums
     when '3'
@@ -23,19 +25,19 @@ class ReportCreator < ApplicationService
 
   private
 
-  def deposition_amount(users_ids = %w[CROOK054130SZ8SK WALTE608284TU2XC], time_period = Time.now - 1.year..Time.now)
+  def deposition_amount(users_ids = User.pluck(:identify_number) , time_period = Time.now - 1.year..Time.now)
     Account.joins(:transaction_log, :user).where(user: { identify_number: users_ids }, transaction_log: { created_at: time_period }).where('transaction_log.sum > 0').group(
       :identify_number, :currency
     ).sum(:sum).to_a
   end
 
-  def extremums(tag = %w[sangria_1 green_2], time_period = Time.now - 1.year..Time.now)
+  def extremums(tag = Tag.pluck(:title), time_period = Time.now - 1.year..Time.now)
     Account.joins(:transaction_log, user: :tags).where(tags: { title: tag }, transaction_log: { created_at: time_period }).where('transaction_log.sum > 0').group(:title, :currency).select(
       'MAX(sum) AS max', 'MIN(sum) AS min', 'AVG(sum) AS avg', 'title', 'currency'
     )
   end
 
-  def all_accounts_sum(tag = 'sangria_1')
+  def all_accounts_sum(tag = Tag.pluck(:title))
     Account.joins(user: :tags).where(tags: { title: tag }).group(:title, :currency).sum(:amount)
   end
 end
